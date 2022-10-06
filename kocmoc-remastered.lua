b3fc1fd95b09223139f28a510e9caaacb9b14a24
ComwareWW <115108976+ComwareWW@users.noreply.github.com>
2022-10-06 08:38:58 +0000

    Update kocmoc-remastered.lua

--- kocmoc-remastered.lua
+++ kocmoc-remastered.lua
@@ -1,14 +1,15 @@
 --[[
 
-// Kocmoc Gen 3 \\
+// Cumware Gen 3 \\
 
 Credits:
-[Boxking776]: Owner, developer, pet simulator x, bee swarm simulator
-[Weuz]: Bee Swarm Simulator, function library (xlp)
-[Devixl]: Bee Swarm Simulator, function library (xlp)
+[NotBad]: Owner, developer of bee swarm simulator
+[Piekarnik]: Bee Swarm Simulator, Manager Function Liblary
+[CUMMYNIGA69]: Bee Swarm Simulator, Kersher Owner
+[Kaspersky652] BSS Starer lol
 
-Kocmoc is owned, coded developed and managed by Boxking776 alone. Anyone else who claims to be a developer is an impersonator.
-Kocmoc is mostly open source apart from some premium functions.
+Cumware is owned, coded developed and managed by NotBad alone. Anyone else who claims to be a developer is an impersonator.
+Cumware is mostly open source apart from some premium functions.
 Any attempts at stealing Kocmoc source and using it without credit will result in a valid takedown request *.
 
 * In moderation. You can use sections of code, however you are prohibited from ripping off the entire source as your own.
@@ -22,5 +23,5 @@ if game.PlaceId == 1537690962 then
 elseif game.PlaceId == 6284583030 or game.PlaceId == 7722306047 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Boxking776/kocmoc/main/games/psx.lua"))()
 else
-	game.Players.LocalPlayer:Kick("Error loadng kocmoc.\n\nNo supported game found.\n\nPlease join a supported game.")
+	game.Players.LocalPlayer:Kick("Error loadng CumWare.\n\nNo supported game found.\n\nPlease join a supported game.")
 end
